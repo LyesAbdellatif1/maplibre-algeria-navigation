1dd2465572294680768e4954ff2ee7ee2643af6e
abdellatif lyes <l.abdellatif@intervalle-technologies.com>
2026-04-20 16:32:58 +0100

    Initial project setup with MapLibre

--- examples/shared/src/examples/Map/ShowMap.tsx
+++ examples/shared/src/examples/Map/ShowMap.tsx
@@ -1,7 +1,23 @@
-import { MapView } from "@maplibre/maplibre-react-native";
-
+import { MapView, PointAnnotation } from "@maplibre/maplibre-react-native";
+import { View } from "react-native";
 import { sheet } from "../../styles/sheet";
 
 export function ShowMap() {
-  return <MapView style={sheet.matchParent} />;
+  return (
+    <MapView
+      style={sheet.matchParent}
+      mapStyle="http://192.168.1.27:8080/styles/basic/style.json"
+    >
+      <PointAnnotation id="test" coordinate={[3.0588, 36.7538]}>
+        <View
+          style={{
+            width: 12,
+            height: 12,
+            backgroundColor: "red",
+            borderRadius: 6,
+          }}
+        />
+      </PointAnnotation>
+    </MapView>
+  );
 }
